66727e621192465aab501779f749c7495c982079
Dandymay <dandyainul19@gmail.com>
2024-07-08 16:38:52 +0700

    hotfix: fixing login page layout

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -4,8 +4,8 @@
 @section('content')
     <div class="relative container flex min-h-screen justify-center items-center">
         <div
-            class="flex flex-col w-11/12 md:w-4/12 items-center text-center bg-[#F9F9F9] border border-gray-200 shadow-md rounded-xl p-10">
-            <img src="{{ asset('assets/Logo Desa.svg') }}" width="200" alt="">
+            class="flex flex-col w-11/12 md:w-4/12 items-center text-center bg-[#F9F9F9] border border-gray-200 shadow-md rounded-xl px-10 pt-2 pb-10">
+            <img src="{{ asset('assets/Logo Desa.svg') }}" width="200" class="mb-2" alt="">
             <h1 class="font-bold text-2xl">Selamat Datang !</h1>
             {{-- <form action="" method="POST"> --}}
             <div class="mt-6 mb-10 w-full text-start">
@@ -13,7 +13,7 @@
                 <input type="text" id="input-label"
                     class=" mb-4 py-3 px-4 block w-full bg-[#f2f2f2] border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none "
                     placeholder="Masukkan Username">
-                <label for="input-label" class="block text-sm font-medium mb-2 text-black">Email</label>
+                <label for="input-label" class="block text-sm font-medium mb-2 text-black">Password</label>
                 <input type="password" id="input-label"
                     class="py-3 px-4 block w-full bg-[#f2f2f2] border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none "
                     placeholder="Masukkan Password">
