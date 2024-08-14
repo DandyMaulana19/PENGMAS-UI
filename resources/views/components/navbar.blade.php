be189384a4401c92a6d1c0812d3b4c1e963258b5
Dandymay <dandyainul19@gmail.com>
2024-08-14 22:51:08 +0700

    slicing: permohonan pindah masuk page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<header class="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3">
+<header class="shadow-md relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3">
     <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
         <div class="flex items-center justify-between">
             <a class="flex-none text-xl font-semibold focus:outline-none focus:opacity-80" href="#"
--- resources/views/pages/pindah-masuk.blade.php
+++ resources/views/pages/pindah-masuk.blade.php
@@ -3,5 +3,104 @@
 @section('title', 'Pindah Masuk')
 
 @section('content')
+    <div class="container py-12">
+        <div class="w-full p-6 bg-white rounded-xl shadow-md">
+            <h1 class="font-bold text-2xl mb-6">Pengajuan Permohonan Pindah Masuk</h1>
+            <hr class="w-full border border-[#9B1010] mb-6">
+            <div class="max-w-full border border-[#9B1010] rounded-md mb-6">
+                <!-- SearchBox -->
+                <div class="relative"
+                    data-hs-combo-box='{
+                  "groupingType": "default",
+                  "isOpenOnFocus": true,
+                  "apiUrl": "../assets/data/searchbox.json",
+                  "apiGroupField": "category",
+                  "outputItemTemplate": "<div data-hs-combo-box-output-item><span class=\"flex items-center cursor-pointer py-2 px-4 w-full text-sm text-gray-800 hover:bg-gray-100"><div class=\"flex items-center w-full\"><div class=\"flex items-center justify-center rounded-full bg-gray-200 size-6 overflow-hidden me-2.5\"><img class=\"shrink-0\" data-hs-combo-box-output-item-attr=&apos;[{\"valueFrom\": \"image\", \"attr\": \"src\"}, {\"valueFrom\": \"name\", \"attr\": \"alt\"}]&apos; /></div><div data-hs-combo-box-output-item-field=\"name\" data-hs-combo-box-search-text data-hs-combo-box-value></div></div><span class=\"hidden hs-combo-box-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"></polyline></svg></span></span></div>",
+                  "groupingTitleTemplate": "<div class=\"text-xs uppercase text-gray-500 m-3 mb-1"></div>"
+                }'>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5">
+                            <svg class="shrink-0 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24"
+                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </svg>
+                        </div>
+                        <input
+                            class="py-3 ps-10 pe-4 block w-full border-[#9B1010] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                            type="text" role="combobox" aria-expanded="false" placeholder="Masukkan nomor KK"
+                            value="" data-hs-combo-box-input="">
+                    </div>
 
+                    <!-- SearchBox Dropdown -->
+                    <div class="absolute z-50 w-full bg-white border border-[#9B1010] rounded-lg" style="display: none;"
+                        data-hs-combo-box-output="">
+                        <div class="max-h-72 rounded-b-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300"
+                            data-hs-combo-box-output-items-wrapper=""></div>
+                    </div>
+                    <!-- End SearchBox Dropdown -->
+                </div>
+                <!-- End SearchBox -->
+            </div>
+
+            <div class="mb-3 text-end">
+                <a href=""
+                    class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-normal rounded-lg border bg-[#922E2C] text-white shadow-sm hover:bg-[#722423] focus:outline-none disabled:opacity-50 disabled:pointer-events-none ">Tambah
+                    Data</a>
+            </div>
+
+            <div class="flex flex-col">
+                <div class="-m-1.5 overflow-x-auto">
+                    <div class="p-1.5 min-w-full inline-block align-middle">
+                        <div class="border overflow-hidden">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-100">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">No.
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">Nama
+                                            Lengkap</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">Jenis
+                                            Kelamin</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">
+                                            Tempat
+                                            Lahir</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">
+                                            Tanggal
+                                            Lahir</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">Agama
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-start text-xs font-semibold text-black uppercase">
+                                            Pendidikan</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">1</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">Hendrik Hermawan</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">Laki-laki</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">Surabaya</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">14 Agustus 2002</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">Islam</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-xs text-black">SMA</td>
+                                    </tr>
+
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
