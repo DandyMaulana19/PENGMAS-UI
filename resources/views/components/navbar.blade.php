62453ca3a129d1072b42cc14d07df12db3f4f100
Dandymay <dandyainul19@gmail.com>
2024-08-15 19:58:36 +0700

    fix: fixing navbar style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<header class="shadow-md relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3">
+<header class="px-6 shadow-md relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3">
     <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
         <div class="flex items-center justify-between">
             <a class="flex-none text-xl font-semibold focus:outline-none focus:opacity-80" href="#"
@@ -28,7 +28,40 @@
                 </button>
             </div>
         </div>
-        <div id="hs-navbar-example"
+        <div class="flex gap-2 items-center justify-center">
+            <span class="text-[#9B1010] text-xs font-medium">Username</span>
+            <span class="inline-block size-[46px] bg-gray-100 rounded-full overflow-hidden">
+                <svg class="size-full text-gray-300" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.62854" y="0.359985" width="15" height="15" rx="7.5" fill="gray"></rect>
+                    <path
+                        d="M8.12421 7.20374C9.21151 7.20374 10.093 6.32229 10.093 5.23499C10.093 4.14767 9.21151 3.26624 8.12421 3.26624C7.0369 3.26624 6.15546 4.14767 6.15546 5.23499C6.15546 6.32229 7.0369 7.20374 8.12421 7.20374Z"
+                        fill="currentColor"></path>
+                    <path
+                        d="M11.818 10.5975C10.2992 12.6412 7.42106 13.0631 5.37731 11.5537C5.01171 11.2818 4.69296 10.9631 4.42107 10.5975C4.28982 10.4006 4.27107 10.1475 4.37419 9.94123L4.51482 9.65059C4.84296 8.95684 5.53671 8.51624 6.30546 8.51624H9.95231C10.7023 8.51624 11.3867 8.94749 11.7242 9.62249L11.8742 9.93184C11.968 10.1475 11.9586 10.4006 11.818 10.5975Z"
+                        fill="currentColor"></path>
+                </svg>
+            </span>
+            <div class="hs-dropdown relative inline-flex">
+                <button id="hs-dropdown-default" type="button"
+                    class="hs-dropdown-toggle pr-1 inline-flex items-center gap-x-2 text-sm font-medium text-gray-800 shadow-sm"
+                    aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                    <svg class="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path d="m6 9 6 6 6-6" />
+                    </svg>
+                </button>
+
+                <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-fit bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
+                    role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
+                    <a class="flex items-center py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
+                        href="#">
+                        Logout
+                    </a>
+                </div>
+            </div>
+            {{-- <div id="hs-navbar-example"
             class="hs-collapse overflow-hidden transition-all duration-300 basis-full grow hidden"
             aria-labelledby="hs-navbar-example-collapse">
             <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
@@ -40,6 +73,6 @@
                 <a class="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400"
                     href="#">Blog</a>
             </div>
-        </div>
+        </div> --}}
     </nav>
 </header>
